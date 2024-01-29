754a4bdf067e370a1b5aee1520ad04295141265e
macrespo42 <macrespo@student.42.fr>
2024-01-29 17:53:21 +0100

    feat: allow chat access to auth user only

--- d09/chat/views.py
+++ d09/chat/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import Chatroom
 
 
+@login_required(login_url="/account")
 def index(request):
     context = {
         "chatrooms": Chatroom.objects.values("name")
@@ -10,5 +12,6 @@ def index(request):
     return render(request, "chat/index.html", context)
 
 
+@login_required(login_url="/account")
 def room(request, name):
     return render(request, "chat/room.html", {"room_name": name})
