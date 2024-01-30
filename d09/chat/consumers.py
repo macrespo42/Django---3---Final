e7bfceaeaffbdf11b27a34ea54e5a4397c74d8be
macrespo42 <macrespo@student.42.fr>
2024-01-30 14:11:21 +0100

    feat: migrate consumer code in async

--- d09/chat/consumers.py
+++ d09/chat/consumers.py
@@ -1,16 +1,19 @@
 import json
 
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+# from django.contrib.auth import get_user_model
+#
+# from .models import Chatroom, Message
 
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
 
@@ -19,45 +22,45 @@ class ChatConsumer(WebsocketConsumer):
         if username:
             username = username.username
             # Send a special message when a user joins the room
-            self.send_special_message(f"{username} joined the room")
+            await self.send_special_message(f"{username} joined the room")
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, _):
+    async def disconnect(self, _):
         username = self.scope.get("user", None)
         if username:
             username = username.username
             # Send a special message when a user joins the room
-            self.send_special_message(f"{username} leave the room")
+            await self.send_special_message(f"{username} leave the room")
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
         # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat.message", "message": message}
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         message = event["message"]
 
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
 
-    def send_special_message(self, message):
+    async def send_special_message(self, message):
         # Send a special message to the room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name, {"type": "chat.special_message", "message": message}
         )
 
-    def chat_special_message(self, event):
+    async def chat_special_message(self, event):
         # Handle special messages (e.g., user joined the room)
         message = event["message"]
 
         # Send the special message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
--- d09/chat/models.py
+++ d09/chat/models.py
@@ -1,6 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Chatroom(models.Model):
 
     name = models.CharField(max_length=42, unique=True)
+
+
+class Message(models.Model):
+
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="author")
+    chatroom = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_ad = models.DateTimeField(auto_now_add=True)
