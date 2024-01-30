3c3ce09d0a62bb4987b39b4e844c579adfe96bc2
macrespo42 <macrespo@student.42.fr>
2024-01-30 15:29:51 +0100

    feat: add messages historic when user joins a room

--- d09/chat/consumers.py
+++ d09/chat/consumers.py
@@ -1,13 +1,22 @@
 import json
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 
-# from django.contrib.auth import get_user_model
-#
-# from .models import Chatroom, Message
+from .models import Chatroom, Message
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    @database_sync_to_async
+    def get_last_messages(self):
+        last_messages = Message.objects.filter(chatroom__name=self.room_name).order_by("-created_ad").values("content")[:3]
+        return [message for message in last_messages]
+
+    @database_sync_to_async
+    def create_chat(self, content, author):
+        chatroom = Chatroom.objects.get(name=self.room_name)
+        return Message.objects.create(author=author, chatroom=chatroom, content=content)
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
@@ -49,6 +58,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event["message"]
 
+        user = self.scope.get("user", None)
+        if user:
+            await self.create_chat(message, user)
+
         # Send message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
 
@@ -60,7 +73,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def chat_special_message(self, event):
         # Handle special messages (e.g., user joined the room)
+
         message = event["message"]
+
+        messages_historic = await self.get_last_messages()
+        if messages_historic:
+            messages_historic.reverse()
+        for old_message in messages_historic:
+            await self.send(text_data=json.dumps({"message": old_message['content']}))
 
         # Send the special message to WebSocket
         await self.send(text_data=json.dumps({"message": message}))
