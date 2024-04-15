1cc1be4bbef9aba5d11afa34f4582b0d6002d9f4
macrespo42 <macrespo@student.42.fr>
2024-04-15 15:40:15 +0200

    fix: history contain only 3 last messages

--- d09/chat/views.py
+++ d09/chat/views.py
@@ -6,19 +6,23 @@ from .models import Chatroom, Message, RoomConnectedUser
 
 @login_required(login_url="/account")
 def index(request):
-    context = {
-        "chatrooms": Chatroom.objects.values("name")
-    }
+    context = {"chatrooms": Chatroom.objects.values("name")}
     return render(request, "chat/index.html", context)
 
 
 @login_required(login_url="/account")
 def room(request, name):
-    messages = Message.objects.filter(chatroom__name=name).order_by("created_ad").values("content")
-    connected_users = RoomConnectedUser.objects.filter(chatroom__name=name).values("user__username")
+    messages = (
+        Message.objects.filter(chatroom__name=name)
+        .order_by("created_ad")
+        .values("content")[2::]
+    )
+    connected_users = RoomConnectedUser.objects.filter(chatroom__name=name).values(
+        "user__username"
+    )
     context = {
         "room_name": name,
         "message_historic": messages,
-        "connected_users": connected_users
+        "connected_users": connected_users,
     }
     return render(request, "chat/room.html", context)
